aa1f87485329319d7a0500565d4dd1bc7ed7459e
Alexsei Popov <popov2895@gmail.com>
2023-11-06 20:26:05 +0300

    add method to get user info

--- school_service/school_service_pb2.py
+++ school_service/school_service_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14school_service.proto\x12\x0eschool_service\"6\n\x10GetSchoolRequest\x12\x10\n\x08username\x18\x01 \x01(\t\x12\x10\n\x08password\x18\x02 \x01(\t\"\xab\x01\n\x11GetSchoolResponse\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x01 \x01(\t\x12\x15\n\rrefresh_token\x18\x02 \x01(\t\x12\x15\n\rsession_state\x18\x03 \x01(\t\x12\x12\n\nexpires_in\x18\x04 \x01(\x05\x12\x16\n\x0eschool_user_id\x18\x05 \x01(\t\x12\x11\n\tcoalition\x18\x06 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x07 \x01(\t2i\n\rSchoolService\x12X\n\x0fget_school_info\x12 .school_service.GetSchoolRequest\x1a!.school_service.GetSchoolResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14school_service.proto\x12\x0eschool_service\"6\n\x10GetSchoolRequest\x12\x10\n\x08username\x18\x01 \x01(\t\x12\x10\n\x08password\x18\x02 \x01(\t\"\xab\x01\n\x11GetSchoolResponse\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x01 \x01(\t\x12\x15\n\rrefresh_token\x18\x02 \x01(\t\x12\x15\n\rsession_state\x18\x03 \x01(\t\x12\x12\n\nexpires_in\x18\x04 \x01(\x05\x12\x16\n\x0eschool_user_id\x18\x05 \x01(\t\x12\x11\n\tcoalition\x18\x06 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x07 \x01(\t\"$\n\x0cGetRpRequest\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x01 \x01(\t\"}\n\rGetRpResponse\x12\x0b\n\x03\x63rp\x18\x01 \x01(\x05\x12\r\n\x05\x63oins\x18\x02 \x01(\x05\x12\x0b\n\x03prp\x18\x03 \x01(\x05\x12\r\n\x05level\x18\x04 \x01(\x05\x12\x12\n\nfirst_name\x18\x05 \x01(\t\x12\x11\n\tlast_name\x18\x06 \x01(\t\x12\r\n\x05login\x18\x07 \x01(\t2\xb7\x01\n\rSchoolService\x12X\n\x0fget_school_info\x12 .school_service.GetSchoolRequest\x1a!.school_service.GetSchoolResponse\"\x00\x12L\n\x0bget_rp_info\x12\x1c.school_service.GetRpRequest\x1a\x1d.school_service.GetRpResponse\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -24,6 +24,10 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_GETSCHOOLREQUEST']._serialized_end=94
   _globals['_GETSCHOOLRESPONSE']._serialized_start=97
   _globals['_GETSCHOOLRESPONSE']._serialized_end=268
-  _globals['_SCHOOLSERVICE']._serialized_start=270
-  _globals['_SCHOOLSERVICE']._serialized_end=375
+  _globals['_GETRPREQUEST']._serialized_start=270
+  _globals['_GETRPREQUEST']._serialized_end=306
+  _globals['_GETRPRESPONSE']._serialized_start=308
+  _globals['_GETRPRESPONSE']._serialized_end=433
+  _globals['_SCHOOLSERVICE']._serialized_start=436
+  _globals['_SCHOOLSERVICE']._serialized_end=619
 # @@protoc_insertion_point(module_scope)
--- school_service/school_service_pb2_grpc.py
+++ school_service/school_service_pb2_grpc.py
@@ -19,12 +19,23 @@ class SchoolServiceStub(object):
                 request_serializer=school__service__pb2.GetSchoolRequest.SerializeToString,
                 response_deserializer=school__service__pb2.GetSchoolResponse.FromString,
                 )
+        self.get_rp_info = channel.unary_unary(
+                '/school_service.SchoolService/get_rp_info',
+                request_serializer=school__service__pb2.GetRpRequest.SerializeToString,
+                response_deserializer=school__service__pb2.GetRpResponse.FromString,
+                )
 
 
 class SchoolServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
 
     def get_school_info(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def get_rp_info(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
@@ -37,6 +48,11 @@ def add_SchoolServiceServicer_to_server(servicer, server):
                     servicer.get_school_info,
                     request_deserializer=school__service__pb2.GetSchoolRequest.FromString,
                     response_serializer=school__service__pb2.GetSchoolResponse.SerializeToString,
+            ),
+            'get_rp_info': grpc.unary_unary_rpc_method_handler(
+                    servicer.get_rp_info,
+                    request_deserializer=school__service__pb2.GetRpRequest.FromString,
+                    response_serializer=school__service__pb2.GetRpResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -62,5 +78,22 @@ class SchoolService(object):
         return grpc.experimental.unary_unary(request, target, '/school_service.SchoolService/get_school_info',
             school__service__pb2.GetSchoolRequest.SerializeToString,
             school__service__pb2.GetSchoolResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def get_rp_info(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/school_service.SchoolService/get_rp_info',
+            school__service__pb2.GetRpRequest.SerializeToString,
+            school__service__pb2.GetRpResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
